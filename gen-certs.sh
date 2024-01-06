47e7310c16fca4a6dc47d5c687fae05135456446
Vemahk <vemahk20@gmail.com>
2024-01-05 22:40:04 -0600

    this works

---
+++ gen-certs.sh
@@ -0,0 +1,7 @@
+openssl req -x509 \
+    -days 3650 -nodes \
+    -newkey rsa:4096 \
+    -keyout vemahk.me.key \
+    -out vemahk.me.crt \
+    -subj /CN=vemahk.me
+#    -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 \
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,80 @@
 const std = @import("std");
+const zap = @import("zap");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn on_request_verbose(r: zap.SimpleRequest) void {
+    if (r.path) |the_path| {
+        std.debug.print("PATH: {s}\n", .{the_path});
+    }
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    if (r.query) |the_query| {
+        std.debug.print("QUERY: {s}\n", .{the_query});
+    }
+    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn on_request_minimal(r: zap.SimpleRequest) void {
+    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
+}
+
+fn help_and_exit(filename: []const u8, err: anyerror) void {
+    std.debug.print(
+        \\ Error: File `{s}` : {any}
+        \\
+        \\ To generate both the certificate file and the key file, use the following command:
+        \\
+        \\ **********************************************************************************************
+        \\ ./gen-certs.sh
+        \\ **********************************************************************************************
+        \\
+        \\ After that, run this example again
+    ,
+        .{ filename, err },
+    );
+    std.os.exit(1);
+}
+pub fn main() !void {
+    const CERT_FILE = "vemahk.me.crt";
+    const KEY_FILE = "vemahk.me.key";
+
+    std.fs.cwd().access(CERT_FILE, .{}) catch |err| {
+        help_and_exit(CERT_FILE, err);
+    };
+
+    std.fs.cwd().access(KEY_FILE, .{}) catch |err| {
+        help_and_exit(KEY_FILE, err);
+    };
+
+    const tls = try zap.Tls.init(.{
+        .server_name = "localhost:4443",
+        .public_certificate_file = CERT_FILE,
+        .private_key_file = KEY_FILE,
+    });
+    defer tls.deinit();
+
+    var listener = zap.SimpleHttpListener.init(.{
+        .port = 4443,
+        .on_request = on_request_verbose,
+        .log = true,
+        .max_clients = 100000,
+        .tls = tls,
+    });
+    try listener.listen();
+
+    std.debug.print("Listening on 0.0.0.0:4443\n", .{});
+    std.debug.print("", .{});
+    std.debug.print(
+        \\
+        \\   ***********************************************
+        \\   *** Try me with: curl -k -v localhost:4443/ ***
+        \\   ***********************************************
+        \\
+        \\Your browser may lie to you, indicate a non-secure connection because of the self-created certificate, and make you believe that HTTPS / TLS "does not work".
+        \\
+    , .{});
+
+    // start worker threads
+    zap.start(.{
+        .threads = 2,
+        .workers = 1,
+    });
 }
